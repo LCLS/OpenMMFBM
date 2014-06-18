3d1657cfe75b43044e3c2f752323465c84246bd1
James Sweet <james.sweet88@googlemail.com>
2014-06-18 11:04:32 -0400

    Removed an extra namespace layer that was not needed from QRTest

--- test/include/QRTest.h
+++ test/include/QRTest.h
@@ -4,20 +4,18 @@
 #include <cppunit/extensions/HelperMacros.h>
 
 namespace FBM {
-	namespace QR {
-		class Test : public CppUnit::TestFixture  {
-			private:
-				CPPUNIT_TEST_SUITE(Test);
-				CPPUNIT_TEST(QRStep);
-                CPPUNIT_TEST(SingleMatrix4);
-				CPPUNIT_TEST(MultipleMatrix4);
-				CPPUNIT_TEST_SUITE_END();
-			public:
-				void QRStep();
-				void SingleMatrix4();
-				void MultipleMatrix4();
-		};
-	}
+	class QR : public CppUnit::TestFixture  {
+		private:
+			CPPUNIT_TEST_SUITE(QR);
+			CPPUNIT_TEST(QRStep);
+			CPPUNIT_TEST(SingleMatrix4);
+			CPPUNIT_TEST(MultipleMatrix4);
+			CPPUNIT_TEST_SUITE_END();
+		public:
+			void QRStep();
+			void SingleMatrix4();
+			void MultipleMatrix4();
+	};
 }
 
 #endif  // OPENMM_FBM_QRTEST_H_
